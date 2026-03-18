9d31299158b323c112f8fe96a9b64e1468cee920
Harpreet Singh <hsingh96@myseneca.ca>
2026-03-18 18:54:56 -0400

    First file added

---
+++ workspace/Pass_ex.py
@@ -0,0 +1,14 @@
+class EmptyClass:
+    pass  # No methods or attributes yet
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        pass  # Placeholder for greet method
+
+# Creating an instance of the class
+p = Person("Emily", 30)
+print(Person.name, Person.age)
