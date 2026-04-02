9d7cfad131f2b6babf2b512e0762c98c7b6b3502
Harpreet Singh <harpreet.moga@gmail.com>
2026-04-02 22:50:27 +0000

    Added Comments

--- OOPS/Lesson 2/SecondOrderSystem.py
+++ OOPS/Lesson 2/SecondOrderSystem.py
@@ -10,3 +10,13 @@ class SecondOrderSystem:
 
 system = SecondOrderSystem(wn=10, zeta=0.5)
 print(f"Damped Frequency: {system.damped_frequency():.2f} rad/s")
+
+
+("""Here:
+
+wn and zeta are attributes because they are parameters
+damped_frequency() is a method because it computes something from them
+
+Do not store everything as an attribute if it is just derived from other values.
+
+That leads to duplicated state and bugs.""")
